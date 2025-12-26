a814657edb3ffb01848cdbd32823d60170987b68
Daheng Wang <wangdaheng2.29@gmail.com>
2025-12-26 14:21:53 -0500

    Add files via upload

--- computer.js
+++ computer.js
@@ -1,23 +1,3 @@
-const follower = document.querySelectorAll(".cursorfollower");
-let timetracker;
-
-document.addEventListener("mousemove", (e) => {
-
-  const xdirection = e.clientX;
-  const ydirection = e.clientY;
-
-  clearTimeout(timetracker);
-  timetracker = setTimeout(() => {
-
-    let count = 1;
-    follower.forEach(element => {
-      element.style.transform = `translate(${xdirection - 23 + count}px, ${ydirection + 32 + count}px)`;
-      count++;
-    });
-
-  }, 100);
-});
-
 window.addEventListener("scroll", () => {
   const sidebar = document.querySelector(".leftbar");
   if (window.scrollY > 100) {
